4d10bca996dab8acd5932dba946bb7d99b9b4a16
dvoronov <voronov-d88@yandex.ru>
2017-05-19 19:59:01 +0300

    Refactoring

--- github_trending.py
+++ github_trending.py
@@ -1,53 +1,46 @@
 import requests
 import datetime
-import time
 
-def fetch_gitreps_created_last_week():
-    search_date = format_day_for_search(get_last_week_day())
-    search_params = {'q': 'created:>={0}'.format(search_date),
-                     'sort': 'stars', 'order': 'desc'}
+
+def fetch_git_repositories(search_params):
     api_git_http = 'https://api.github.com/search/repositories'
     request = requests.get(api_git_http, params=search_params)
-    if not request.raise_for_status():
+    if request.status_code == requests.codes.ok:
         return request.json()
 
 
-def get_last_week_day():
-    count_days_for_adjust = 7
+def fetch_gitreps_created_recently(count_days_adjust):
+    search_date = format_day_for_search(get_last_day(count_days_adjust))
+    search_params = {'q': 'created:>={0}'.format(search_date),
+                     'sort': 'stars', 'order': 'desc'}
+    return fetch_git_repositories(search_params)
+
+
+def get_last_day(count_days_adjust):
     today = datetime.date.today()
-    return today - datetime.timedelta(days=count_days_for_adjust)
+    return today - datetime.timedelta(days=count_days_adjust)
 
 
 def format_day_for_search(search_date):
     return search_date.isoformat()
 
 
-def get_trending_repositories(top_size):
-    repositories = fetch_gitreps_created_last_week()
+def get_trending_repositories(top_size, count_days_adjust):
+    repositories = fetch_gitreps_created_recently(count_days_adjust)
     return repositories['items'][:top_size]
-
-
-def fetch_open_issues_amount(repo_owner, repo_name):
-    time.sleep(1)
-    request = requests.get('https://api.github.com/repos/{0}/{1}/issues'
-                     .format(repo_owner, repo_name))
-    if request.raise_for_status():
-        return None
-    return len(request.json())
 
 
 def print_git_repositories_short_information(repositories):
     for repos in repositories:
-        repos_owner = repos['owner']['login']
-        repos_name = repos['name']
-        open_issues_amount = fetch_open_issues_amount(repos_owner, repos_name)
         print('Name: {0}'.format(repos['name']))
         print('Description: {0}'.format(repos['description']))
         print('URL: {0}'.format(repos['html_url']))
-        print('Open issues amount: {0}'.format(open_issues_amount))
+        print('Open issues amount: {0}'.format(repos['open_issues']))
+        print('')
 
 
 if __name__ == '__main__':
     amount_repositories = 20
-    repositories = get_trending_repositories(amount_repositories)
+    one_week = 7
+    repositories = get_trending_repositories(amount_repositories, one_week)
     print_git_repositories_short_information(repositories)
